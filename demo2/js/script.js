1f69c735ddb7c0e927af5df4c44b659946eac888
unknown <likhonsorkar02@yahoo.com>
2022-12-16 12:38:29 +0600

    commit message

--- demo2/js/script.js
+++ demo2/js/script.js
@@ -4,3 +4,17 @@ menuBar.addEventListener('click', function (){
     menuBar.classList.toggle('fa-times');
     menuLink.classList.toggle('active');
 });
+// Swiper
+var swiper = new Swiper(".main-slide", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   loop:true,
+  });
