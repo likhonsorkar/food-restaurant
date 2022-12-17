8e151f9d6f3b2e4cd3505c2f3eb5ec29ab6f90bc
unknown <likhonsorkar02@yahoo.com>
2022-12-17 12:35:38 +0600

    food resurent website complete

--- demo2/js/script.js
+++ demo2/js/script.js
@@ -18,3 +18,37 @@ var swiper = new Swiper(".main-slide", {
     },
    loop:true,
   });
+  // Swiper
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+ loop:true,
+ breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1.7,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+});
+// Loader
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
